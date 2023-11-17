2a125aa9b7a9ee4528bf1e82ddab1cde26660c48
Ayman Ragab <45921642+aymanrgab@users.noreply.github.com>
2023-11-17 21:46:17 +0000

    update

--- build/build-kernel.sh
+++ build/build-kernel.sh
@@ -3,19 +3,14 @@ set -ex
 
 TMPDOWN=$1
 INSTALL_MOD_PATH=$2
-if [ "$TARGET_DISTRO" == "focal" ]; then
-    # Modules go under /usr/lib
-    INSTALL_MOD_PATH="$INSTALL_MOD_PATH/usr"
-fi
 HERE=$(pwd)
 source "${HERE}/deviceinfo"
 
 KERNEL_DIR="${TMPDOWN}/$(basename "${deviceinfo_kernel_source}")"
-KERNEL_DIR="${KERNEL_DIR%.*}"
+KERNEL_DIR="${KERNEL_DIR%.git}"
 OUT="${TMPDOWN}/KERNEL_OBJ"
 
 mkdir -p "$OUT"
-mkdir -p "${INSTALL_MOD_PATH}"
 
 case "$deviceinfo_arch" in
     aarch64*) ARCH="arm64" ;;
@@ -25,20 +20,25 @@ case "$deviceinfo_arch" in
 esac
 
 export ARCH
-export CROSS_COMPILE=$TMPDOWN/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=$TMPDOWN/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE="${deviceinfo_arch}-linux-android-"
+if [ "$ARCH" == "arm64" ]; then
+    export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+fi
 MAKEOPTS=""
 if [ -n "$CC" ]; then
     MAKEOPTS="CC=$CC"
 fi
-if [ -n "$HOSTCC" ]; then
-    MAKEOPTS="HOSTCC=$HOSTCC $MAKEOPTS"
+if [ -n "$LD" ]; then
+    MAKEOPTS+=" LD=$LD"
 fi
 
 cd "$KERNEL_DIR"
-make O="$OUT" $deviceinfo_kernel_defconfig
+make O="$OUT" $MAKEOPTS $deviceinfo_kernel_defconfig
 make O="$OUT" $MAKEOPTS -j$(nproc --all)
-make O="$OUT" $MAKEOPTS INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH="$INSTALL_MOD_PATH" modules_install
+if [ "$deviceinfo_kernel_disable_modules" != "true" ]
+then
+    make O="$OUT" $MAKEOPTS INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH="$INSTALL_MOD_PATH" modules_install
+fi
 ls "$OUT/arch/$ARCH/boot/"*Image*
 
 if [ -n "$deviceinfo_kernel_apply_overlay" ] && $deviceinfo_kernel_apply_overlay; then
@@ -47,4 +47,10 @@ if [ -n "$deviceinfo_kernel_apply_overlay" ] && $deviceinfo_kernel_apply_overlay
         "$OUT/arch/arm64/boot/dts/qcom/${deviceinfo_kernel_dtb_overlay}-merged.dtb"
     cat "$OUT/arch/$ARCH/boot/Image.gz" \
         "$OUT/arch/arm64/boot/dts/qcom/${deviceinfo_kernel_dtb_overlay}-merged.dtb" > "$OUT/arch/$ARCH/boot/Image.gz-dtb"
+fi
+
+if [ -n "$deviceinfo_use_overlaystore" ]; then
+    # Config this directory in the overlay store to override (i.e. bind-mount)
+    # the whole directory. Rootfs won't ship any device-specific kernel module.
+    touch "${INSTALL_MOD_PATH}/lib/modules/.halium-override-dir"
 fi
